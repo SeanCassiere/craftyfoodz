b46a8866aa93a7ece74e298344a182c7594a73f1
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-04-20 15:24:52 +1200

    chore(superadmin): folder restructuring for trpc

--- apps/superadmin-web/src/pages/api/trpc/[trpc].ts
+++ apps/superadmin-web/src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,7 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 
-import { appRouter, createTRPCContext } from "~/server/trpc";
+import { appRouter } from "~/server/api/root";
+import { createTRPCContext } from "~/server/api/trpc";
 
 // export API handler
 export default createNextApiHandler({
