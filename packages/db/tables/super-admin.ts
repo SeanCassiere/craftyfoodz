99463b1ee6eea2e34070f4303805cf8b6caa56e9
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-04-20 22:00:32 +1200

    feat(superadmin): add is_active column to super admin table

--- packages/db/tables/super-admin.ts
+++ packages/db/tables/super-admin.ts
@@ -1,9 +1,15 @@
-import { mysqlTable, timestamp, varchar } from "drizzle-orm/mysql-core";
+import {
+  boolean,
+  mysqlTable,
+  timestamp,
+  varchar,
+} from "drizzle-orm/mysql-core";
 
 export const SuperAdminAccount = mysqlTable("super_admin_accounts", {
   id: varchar("id", { length: 20 }).primaryKey(),
   email: varchar("email", { length: 255 }).notNull(),
   name: varchar("name", { length: 255 }).notNull(),
+  is_active: boolean("is_active").default(true),
   updated_at: timestamp("updated_at").notNull().defaultNow().onUpdateNow(),
   created_at: timestamp("created_at").notNull().defaultNow(),
 });
