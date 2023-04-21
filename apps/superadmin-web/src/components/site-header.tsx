8b92df89265ad1af70f4a0a90d447360c383b062
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-04-21 23:05:36 +1200

    fix(superadmin): page header redirect to base pathname

--- apps/superadmin-web/src/components/site-header.tsx
+++ apps/superadmin-web/src/components/site-header.tsx
@@ -33,13 +33,13 @@ export const SiteHeader = ({ pathname }: { pathname: string }) => {
       <header className="relative mx-auto max-w-[1440px]">
         <div className="flex items-center pb-4 pt-4 md:pb-6 md:pt-6">
           <div className="mr-1 flex shrink-0 items-center">
-            <Link href="/dashboard" aria-label="Go to dashboard">
+            <Link href="/" aria-label="Go to the base page">
               <Command className="mr-2 h-8 w-8" />
             </Link>
           </div>
           <div className="flex flex-grow items-center">
             <Link
-              href="/dashboard"
+              href="/"
               className="hidden items-center rounded p-1 text-lg font-medium leading-3 transition sm:flex"
             >
               Admin - {UI_CONFIG.company_name}
