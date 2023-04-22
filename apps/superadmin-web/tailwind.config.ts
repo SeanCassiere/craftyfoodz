51547b24232e59cc6ffeb23d66dbbf18a002a305
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-04-22 15:13:08 +1200

    feat(superadmin): add basic global feature conifguration ui

--- apps/superadmin-web/tailwind.config.ts
+++ apps/superadmin-web/tailwind.config.ts
@@ -33,6 +33,10 @@ export default {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
         },
+        warning: {
+          DEFAULT: "hsl(var(--warning))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
         muted: {
           DEFAULT: "hsl(var(--muted))",
           foreground: "hsl(var(--muted-foreground))",
