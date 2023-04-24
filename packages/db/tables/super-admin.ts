ff13d529e6f4bd1f02c98a12ce1e43729c3ab49b
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-04-24 22:22:59 +1200

    feat(superadmin): add index on the super admin id column of the admin login attempt table

--- packages/db/tables/super-admin.ts
+++ packages/db/tables/super-admin.ts
@@ -1,5 +1,6 @@
 import {
   boolean,
+  index,
   mysqlEnum,
   mysqlTable,
   timestamp,
@@ -16,11 +17,19 @@ export const SuperAdminAccount = mysqlTable("sa_accounts", {
   created_at: timestamp("created_at").notNull().defaultNow(),
 });
 
-export const SuperAdminLoginAttempt = mysqlTable("sa_login_attempts", {
-  id: varchar("id", { length: 20 }).primaryKey(),
-  sa_account_id: varchar("sa_account_id", { length: 20 }).notNull(),
-  access_code: varchar("access_code", { length: 255 }).notNull(),
-  is_expired: boolean("is_expired").default(false),
-  updated_at: timestamp("updated_at").notNull().defaultNow().onUpdateNow(),
-  created_at: timestamp("created_at").notNull().defaultNow(),
-});
+export const SuperAdminLoginAttempt = mysqlTable(
+  "sa_login_attempts",
+  {
+    id: varchar("id", { length: 20 }).primaryKey(),
+    sa_account_id: varchar("sa_account_id", { length: 20 }).notNull(),
+    access_code: varchar("access_code", { length: 255 }).notNull(),
+    is_expired: boolean("is_expired").default(false),
+    updated_at: timestamp("updated_at").notNull().defaultNow().onUpdateNow(),
+    created_at: timestamp("created_at").notNull().defaultNow(),
+  },
+  (loginAttempt) => ({
+    sa_account_id_idx: index("sa_account_id_idx").on(
+      loginAttempt.sa_account_id,
+    ),
+  }),
+);
