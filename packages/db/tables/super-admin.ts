7a1c3659a08c7e5fd0b0a46f11df756d6dc8abec
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-04-24 22:26:27 +1200

    refactor(superadmin): change super_admin role to developer

--- packages/db/tables/super-admin.ts
+++ packages/db/tables/super-admin.ts
@@ -11,7 +11,7 @@ export const SuperAdminAccount = mysqlTable("sa_accounts", {
   id: varchar("id", { length: 20 }).primaryKey(),
   email: varchar("email", { length: 255 }).notNull(),
   name: varchar("name", { length: 255 }).notNull(),
-  role: mysqlEnum("role", ["super_admin", "admin"]).notNull().default("admin"),
+  role: mysqlEnum("role", ["developer", "admin"]).notNull().default("admin"),
   is_active: boolean("is_active").default(true),
   updated_at: timestamp("updated_at").notNull().defaultNow().onUpdateNow(),
   created_at: timestamp("created_at").notNull().defaultNow(),
